fb344a881dd04aa4f27959b64e029678fe65b1ee
Patrick Ugwu <patrick.ugwu@web.de>
2025-03-11 09:19:02 +0100

    add calendar component

---
+++ mybrain/src/app/pages/calendar/calendar.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CalendarService } from '../../services/calendar.service';
+import { Action } from '../../models/interfaces/action.interface';
+import { ToDo } from '../../models/interfaces/todo.interface';
+import { CalenderDayComponent } from "../../components/calendar/calendar-day/calender-day.component";
+
+@Component({
+  selector: 'app-calendar',
+  standalone: true,
+  imports: [CalenderDayComponent],
+  templateUrl: './calendar.component.html',
+  styleUrl: './calendar.component.css'
+})
+export class CalendarComponent implements OnInit {
+  calendarService = inject(CalendarService)
+  actions = signal<Action[]>([])
+  todos = signal<ToDo[]>([])
+  view = signal("day")
+  
+  ngOnInit(): void {
+    this.calendarService.getActions().subscribe(data => {
+      this.actions.set(data)
+    })
+
+    this.calendarService.getToDos().subscribe(data => {
+      this.todos.set(data)
+    })
+  }
+
+  setView(view:string): void {
+    this.view.set(view)
+
+  }
+}
