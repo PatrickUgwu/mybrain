1e33aa02e44e249e3fa0e3339efb3f2a3fdb8515
Patrick Ugwu <patrick.ugwu@web.de>
2025-03-13 08:18:44 +0100

    add day overview to homepage

--- mybrain/src/app/pages/home/home.component.ts
+++ mybrain/src/app/pages/home/home.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CalenderDayComponent } from "../../components/calendar/calendar-day/calender-day.component";
+import { CalendarService } from '../../services/calendar.service';
+import { NgControl } from '@angular/forms';
+import { Action } from '../../models/interfaces/action.interface';
+import { ToDo } from '../../models/interfaces/todo.interface';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CalenderDayComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  calendarService = inject(CalendarService)
+  actions = signal<Action[]>([])
+  todos = signal<ToDo[]>([])
+  
+  ngOnInit(): void {
+    this.calendarService.getActions().subscribe(data => {
+      this.actions.set(data)
+    })
 
+    this.calendarService.getToDos().subscribe(data => {
+      this.todos.set(data)
+    })
+  }
 }
