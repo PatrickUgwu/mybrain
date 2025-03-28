3896d7e471a72fe329966e10ef2382cc0139469f
Patrick Ugwu <patrick.ugwu@web.de>
2025-03-28 15:40:58 +0100

    add month goals to month view

--- mybrain/src/app/components/calendar/calendar-month/calendar-month.component.ts
+++ mybrain/src/app/components/calendar/calendar-month/calendar-month.component.ts
@@ -3,6 +3,7 @@ import { Action } from '../../../models/interfaces/action.interface';
 import { ToDo } from '../../../models/interfaces/todo.interface';
 import { CalendarTileComponent } from '../calendar-tile/calendar-tile.component';
 import { CalendarService } from '../../../services/calendar.service';
+import { Goal } from '../../../models/interfaces/goal.interface';
 
 @Component({
   selector: 'app-calendar-month',
@@ -14,13 +15,27 @@ import { CalendarService } from '../../../services/calendar.service';
 export class CalendarMonthComponent implements OnInit{
   VIEW = "month"
   calendarService = inject(CalendarService)
+  goals = input.required<Goal[]>()
   actions = input.required<Action[]>()
   todos = input.required<ToDo[]>()
-  month:string[] = [];
+  month:[string, Goal[]][] = [];
 
   ngOnInit(): void {
     this.calendarService.getMonthDays().subscribe(data => {
-      this.month = data
+      
+      data.forEach(date => {
+        let day:[string, Goal[]] = ["",[]]
+        day[0] = date
+        
+        this.goals().forEach(goal => {
+          if (date === goal.deadline) {
+            day[1].push(goal)
+          }
+        })
+
+        this.month.push(day)
+      })
+      
     })
   }
 }
