1fa656080feecfd06da099dff8c0f92cc8160c66
Patrick Ugwu <patrick.ugwu@web.de>
2025-04-20 17:20:41 +0200

    remove dependency from tile component for day view

--- mybrain/src/app/components/calendar/calendar-day/calender-day.component.ts
+++ mybrain/src/app/components/calendar/calendar-day/calender-day.component.ts
@@ -2,18 +2,27 @@ import { Component, input } from '@angular/core';
 import { CalendarTileComponent } from "../calendar-tile/calendar-tile.component";
 import { Action } from '../../../models/interfaces/action.interface';
 import { ToDo } from '../../../models/interfaces/todo.interface';
+import { CalendarService } from '../../../services/calendar.service';
+import { CalendarActionComponent } from "../calendar-action/calendar-action.component";
+import { CalendarTodoComponent } from "../calendar-todo/calendar-todo.component";
 
 @Component({
   selector: 'app-calender-day',
   standalone: true,
-  imports: [CalendarTileComponent],
+  imports: [CalendarActionComponent, CalendarTodoComponent],
   templateUrl: './calender-day.component.html',
   styleUrl: './calender-day.component.css'
 })
-export class CalenderDayComponent {
-  VIEW = "day"
+export class CalenderDayComponent implements OnInit{
+  calendarService = inject(CalendarService)
+  weekday = ""
   day = input.required<string>()
   actions = input.required<Action[]>()
   todos = input.required<ToDo[]>()
+  ngOnInit(): void {
+    this.calendarService.getWeekDay(this.day()).subscribe(day => {
+      this.weekday = day
+    })
+  }
 
 }
