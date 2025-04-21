bd845ba4ab2ee164c1b3a2c227f1197c7d953436
Patrick Ugwu <patrick.ugwu@web.de>
2025-04-21 21:40:01 +0200

    integrate add-window into calendar month view

--- mybrain/src/app/components/calendar/calendar-month/calendar-month.component.ts
+++ mybrain/src/app/components/calendar/calendar-month/calendar-month.component.ts
@@ -1,30 +1,38 @@
 import { Component, inject, input, OnInit } from '@angular/core';
 import { Action } from '../../../models/interfaces/action.interface';
 import { ToDo } from '../../../models/interfaces/todo.interface';
-import { CalendarTileComponent } from '../calendar-tile/calendar-tile.component';
 import { CalendarService } from '../../../services/calendar.service';
 import { Goal } from '../../../models/interfaces/goal.interface';
 import { CalendarTodoComponent } from "../calendar-todo/calendar-todo.component";
 import { CalendarGoalComponent } from "../calendar-goal/calendar-goal.component";
+import { AddWindowComponent } from "../../roadmap/add-window/add-window.component";
 
 @Component({
   selector: 'app-calendar-month',
   standalone: true,
-  imports: [CalendarTodoComponent, CalendarGoalComponent],
+  imports: [CalendarTodoComponent, CalendarGoalComponent, AddWindowComponent],
   templateUrl: './calendar-month.component.html',
   styleUrl: './calendar-month.component.css'
 })
 export class CalendarMonthComponent implements OnInit{
-  VIEW = "month"
   calendarService = inject(CalendarService)
   monthGoals = input.required<Goal[]>()
   weekGoals = input.required<Goal[]>()
   actions = input.required<Action[]>()
   month:[[string, ToDo[]][], Goal[]][] = [];
+  add =  false
+
+  openAddWindow() {
+    this.add = true
+  }
+  
+  closeAddWindow() {
+    this.add = false
+  }
 
   ngOnInit(): void {
     this.calendarService.getMonth().subscribe(data => {
-      let week:[[string, ToDo[]][], Goal[]] = [[],[]] // [ [Date, ToDos][], weekGoals[] ]
+      let week:[[string, ToDo[]][], Goal[]] = [[],[]] // [ [Date, ToDos[]][], weekGoals[] ]
       for (let i = 0; i < data.length; i++) {
 
         // add day to week
