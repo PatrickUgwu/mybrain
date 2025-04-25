a3deecb444574e120c188f14edfa129176b71c3f
Patrick Ugwu <patrick.ugwu@web.de>
2025-04-25 22:12:16 +0200

    integrate FormGroup for adding roadmap items

--- mybrain/src/app/components/roadmap/add-window/add-window.component.ts
+++ mybrain/src/app/components/roadmap/add-window/add-window.component.ts
@@ -1,18 +1,46 @@
 import { Component, EventEmitter, Input, input, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-window',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './add-window.component.html',
   styleUrl: './add-window.component.css'
 })
 export class AddWindowComponent {
   item: string = ""
   @Output() close = new EventEmitter<void>()
+  @Input() goals = ""
 
+  itemForm:FormGroup = new FormGroup({
+    type: new FormControl("", [
+      Validators.required
+    ]),
+    title: new FormControl("", [
+      Validators.required
+    ]),
+    description: new FormControl("", [
+      Validators.required, 
+      Validators.minLength(3)
+    ]),
+    parent: new FormControl("", [
+      Validators.required
+    ]),
+    // Actions
+    pattern: new FormControl(""),
+
+    // Goals
+    deadline: new FormControl(""),
+    goalType: new FormControl(""),
+  })
+  
   closeAdd(){
     this.close.emit()
+  }
+
+  addItem() {
+    console.log(this.itemForm)
+    this.closeAdd()
   }
 }
