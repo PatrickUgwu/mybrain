86feb698ad9d1ac100ece8f28269630efefb1126
Patrick Ugwu <patrick.ugwu@web.de>
2025-06-03 21:49:39 +0200

    add overview window for roadmap items

---
+++ mybrain/src/app/components/roadmap/item-overview/item-overview.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject, input, OnInit, output } from '@angular/core';
+import { ToDo } from '../../../models/interfaces/todo.interface';
+import { Goal } from '../../../models/interfaces/goal.interface';
+import { Milestone } from '../../../models/interfaces/milestone.interface';
+import { Action } from '../../../models/interfaces/action.interface';
+import { RoadmapService } from '../../../services/roadmap.service';
+
+@Component({
+  selector: 'app-item-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-overview.component.html',
+  styleUrl: './item-overview.component.css'
+})
+export class ItemOverviewComponent implements OnInit {
+  roadmapService = inject(RoadmapService)
+  itemType = input.required<string>()
+  item = input.required<any>()
+  parent: Milestone | Goal | Action | ToDo | undefined
+  close = output()
+
+  closeAdd(){
+    this.close.emit()
+  }
+
+  ngOnInit(): void {
+    this.roadmapService.getParent(this.itemType(), this.item().id).subscribe(parent => {
+      this.parent = parent
+    })
+  }
+}
