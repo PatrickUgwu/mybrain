194e40a43cc8fcf06d86bbcf497ec702d2576509
Patrick Ugwu <patrick.ugwu@web.de>
2025-06-17 11:00:19 +0200

    add item detail popup to roadmap overview

--- mybrain/src/app/components/roadmap/roadmap-overview/roadmap-overview.component.ts
+++ mybrain/src/app/components/roadmap/roadmap-overview/roadmap-overview.component.ts
@@ -6,11 +6,12 @@ import { Milestone } from '../../../models/interfaces/milestone.interface';
 import { Goal } from '../../../models/interfaces/goal.interface';
 import { Action } from '../../../models/interfaces/action.interface';
 import { ToDo } from '../../../models/interfaces/todo.interface';
+import { ItemOverviewComponent } from "../item-overview/item-overview.component";
 
 @Component({
   selector: 'app-roadmap-overview',
   standalone: true,
-  imports: [],
+  imports: [AddButtonComponent, ItemOverviewComponent],
   templateUrl: './roadmap-overview.component.html',
   styleUrl: './roadmap-overview.component.css'
 })
@@ -22,6 +23,22 @@ export class RoadmapOverviewComponent {
   goals = computed<Goal[]>( () => this.roadmapService.goals() )
   actions = computed<Action[]>( () => this.roadmapService.actions())
   todos = computed<ToDo[]>( () => this.roadmapService.todos() )
+
+  popup = {"display" : false, "itemType" : "", "item" : null}
+
+  openPopup(itemType: string, item: any, event: Event) {
+    event.stopPropagation();
+    this.popup.display = true
+    this.popup.itemType = itemType
+    this.popup.item = item
+  }
+
+  closePopup(): void {
+    this.popup.display = false
+    this.popup.itemType = ""
+    this.popup.item = null
+  }
+
   deleteItem(item: any, itemType: string) {
     this.roadmapService.deleteItem(item.id, itemType)
   }
