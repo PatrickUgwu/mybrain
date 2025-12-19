3552483a85d56e52ed56ca38deea2475e8c96b46
Patrick Ugwu <patrick.ugwu@web.de>
2025-12-19 21:41:53 +0100

    adjust page interface to match the Page SQL-Model

--- mybrain/src/app/models/interfaces/page.interface.ts
+++ mybrain/src/app/models/interfaces/page.interface.ts
@@ -1,5 +1,8 @@
 export interface Page {
+    id?: number,
     title: string,
     content: string,
-    lastEdit: string
+    parent_id: string,
+    first_edit?: string,
+    last_edit?: string
 }
